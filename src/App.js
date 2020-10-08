a237c783615949eebb7cfd10f3d89e86672d0498
Lawrence <Lawrence4code@gmail.com>
2020-10-08 12:47:13 +0530

    chore: basic routing for AlbumsList and single Albums and common bg for app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import AlbumList from './components/AlbumsList/AlbumsList';
+import Album from './components/Album/Album';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> App component </h1>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <AlbumList />
+        </Route>
+        <Route exact path="/album">
+          <AlbumList />
+        </Route>
+        <Route exact path="/album/:id">
+          <Album />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/AlbumsList/AlbumsList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AlbumsList = () => {
+  return <div>AlbumsList component</div>;
+};
+
+export default AlbumsList;
